a21fc90791b4170fc85a7ec897673a0892743392
steeven-hub <wilsonbienvenusogbossi@gmail.com>
2026-05-08 14:33:02 +0100

    chore: add whitenoise and fix dynamic port for render

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ set -o errexit
 echo "Running Migrations..."
 python manage.py migrate --noinput
 
-echo "Starting Server..."
-daphne -b 0.0.0.0 -p 10000 health_platform.asgi:application
+echo "Starting Server on port ${PORT:-10000}..."
+daphne -b 0.0.0.0 -p ${PORT:-10000} health_platform.asgi:application
--- health_platform/settings.py
+++ health_platform/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', # Toujours en premier
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware', 
     'django.middleware.csrf.CsrfViewMiddleware', # Essentiel pour la sécurité des formulaires
@@ -123,6 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
