11e4589f6c40099ce8df37315a2dbc0b1918c64e
steeven-hub <wilsonbienvenusogbossi@gmail.com>
2026-05-08 13:32:21 +0100

    fix: redirect root URL to api home

--- health_platform/urls.py
+++ health_platform/urls.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    # Redirection de la racine vers l'accueil API
+    path('', RedirectView.as_view(url='/api/home/', permanent=True)),
+    
     # Interface d'administration
     path('admin/', admin.site.urls),
     
