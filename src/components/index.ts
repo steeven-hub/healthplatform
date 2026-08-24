ecdfb269a9bf3d25555de15d00ae7cb0fce98ca1
steeven-hub <wilsonbienvenusogbossi@gmail.com>
2026-08-24 23:46:09 +0100

    feat: Add component exports index

---
+++ src/components/index.ts
@@ -0,0 +1,15 @@
+// Button Components
+export { Button, buttonVariants } from './Button/Button';
+
+// Card Components
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from './Card/Card';
+
+// Input Components
+export { Input } from './Input/Input';
