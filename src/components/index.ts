b61ce28b732b7b5022b4d34b0523ac83a556a05d
steeven-hub <wilsonbienvenusogbossi@gmail.com>
2026-08-24 23:54:29 +0100

    feat: Update component exports with all new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -13,3 +13,18 @@ export {
 
 // Input Components
 export { Input } from './Input/Input';
+
+// Label Components
+export { Label } from './Label/Label';
+
+// Badge Components
+export { Badge } from './Badge/Badge';
+
+// Alert Components
+export { Alert } from './Alert/Alert';
+
+// Progress Components
+export { Progress } from './Progress/Progress';
+
+// Avatar Components
+export { Avatar } from './Avatar/Avatar';
